7f0c671eb3d8cb16a3de3f9019a05594c8292ce7
fernando0x40 <fernando0x40@gmail.com>
2019-11-05 16:50:36 -0300

    Analog Read Test

--- WebSensor/src/main.cpp
+++ WebSensor/src/main.cpp
@@ -1,12 +1,23 @@
 #include <Arduino.h>
 
+int leds[3] = {0, 16, 5};
+int LDR = 4;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(LDR, INPUT);
+  for (size_t i = 0; i < sizeof(leds); i++) {
+    pinMode(leds[i], OUTPUT);
+    digitalWrite(leds[i], LOW);
+  }
 }
 
+int LDRvalor = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("teste");
-  delay(500);
+  LDRvalor = analogRead(LDR);
+  Serial.println(LDRvalor);
+  Serial.printf("Valor do LDR = %.1f\n", LDRvalor*3.3/4095.0);
+  delay(200);
 }
